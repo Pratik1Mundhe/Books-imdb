378fe32767946167b0b8fdd423d46cefe5481d26
Hardkpentium <201220033@nitdelhi.ac.in>
2023-01-02 18:32:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react'
+import {Transition} from 'react-transition-group'
+import {GlobalStyle} from './styles'
+import BooksContainer from './components/BooksContainer'
+import Header from './components/Header'
+import DetailPanel from './components/DetailPanel'
+import Search from './components/Search'
+
+const App = () => {
+  const [books, setBooks] = useState([])
+  const [filteredBooks, setFilteredBooks] = useState([])
+  const [selectedBook, setSelectedBook] = useState(null)
+  const [showPanel, setShowPanel] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://book-club-json.herokuapp.com/books')
+      const books = await response.json()
+      setBooks(books)
+      setFilteredBooks(books)
+    }
+
+    fetchData()
+  }, [])
+
+  const pickBook = (book) => {
+    setSelectedBook(book)
+    setShowPanel(true)
+  }
+
+  const closePanel = () => {
+    setShowPanel(false)
+  }
+
+  const filterBooks = (searchTerm) => {
+    const stringSearch = (bookAttribute, searchTerm) =>
+      bookAttribute.toLowerCase().includes(searchTerm.toLowerCase())
+
+    if (!searchTerm) {
+      setFilteredBooks(books)
+    } else {
+      setFilteredBooks(
+        books.filter(
+          (book) => stringSearch(book.title, searchTerm) || stringSearch(book.author, searchTerm)
+        )
+      )
+    }
+  }
+
+  const hasFiltered = filteredBooks.length !== books.length
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header>
+        <Search filterBooks={filterBooks} />
+      </Header>
+      <BooksContainer
+        books={filteredBooks}
+        pickBook={pickBook}
+        isPanelOpen={showPanel}
+        title={hasFiltered ? 'Search results' : 'All books'}
+      />
+      <Transition in={showPanel} timeout={300}>
+        {(state) => <DetailPanel book={selectedBook} state={state} closePanel={closePanel} />}
+      </Transition>
+    </>
+  )
+}
+
+export default App
